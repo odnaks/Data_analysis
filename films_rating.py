7c67c87c08eea6022a8e1742f5f9b724f329c9d8
odnaks <43475234+odnaks@users.noreply.github.com>
2019-03-04 23:21:10 +0300

    04.03.19

---
+++ films_rating.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+""" Расчет и вывод рейтинга для каждого фильма
+       на основе оценок всех пользователей """
+
+data = pd.read_csv('ratings.csv')
+rating_of_movies = data.pivot_table(index = ['movieId', 'rating'],
+    values = 'timestamp', aggfunc = 'count').reset_index()
+rating_of_movies['sum'] = rating_of_movies.groupby('movieId').timestamp.transform(lambda x: sum(x))
+rating_of_movies['mul'] = rating_of_movies['rating'] * rating_of_movies['timestamp']
+rating_of_movies['sum_of_marks'] = rating_of_movies.groupby('movieId').mul.transform(lambda x: sum(x))
+rating_of_movies['rating'] = rating_of_movies['sum_of_marks'] / rating_of_movies['sum']
+del rating_of_movies['timestamp']
+del rating_of_movies['sum']
+del rating_of_movies['mul']
+del rating_of_movies['sum_of_marks']
+rating_of_movies.drop_duplicates('movieId', inplace = True)
+print (rating_of_movies.sort_values('rating', ascending = False))
---
+++ geo_classifier.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+""" Наша задача - написать гео-классификатор, который каждой строке
+сможет выставить географическую принадлежность определенному региону.
+Т. е. если поисковый запрос содержит название города региона,
+то в столбце 'region' пишется название этого региона.
+Если поисковый запрос не содержит названия города, то ставим
+'undefined'. Какое количество строк пришлось на Дальний Восток?"""
+
+geo_data = {
+    'Центр': ['москва', 'тула', 'ярославль'],
+    'Северо-Запад': ['петербург', 'псков', 'мурманск'],
+    'Дальник Восток': ['владивосток', 'сахалин', 'хабаровск']
+}
+
+def geo_classification(row):
+    parsed = row['keyword']
+    for region, city_list in geo_data.items():
+        for city in city_list:
+            if parsed.find(city) != -1:
+                return region
+    return "undefined"
+
+data = pd.read_csv('keywords.csv')
+#print (data.head())
+data['region'] = data.apply(geo_classification, axis = 1)
+data = data.groupby('region').sum()
+print (data.sort_values(by = 'shows'))
