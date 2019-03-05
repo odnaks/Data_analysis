81566b88ed3c3419cea1767b7a93d594ed3d6eb6
odnaks <43475234+odnaks@users.noreply.github.com>
2019-03-05 04:04:31 +0300

    Add files via upload

---
+++ concat_of_files.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+from os.path import join, getsize
+
+""" Напишите цикл, который собирает содержимое
+файлов папки data в единый датафрейм data.
+Сколько строк получится в датафрейме data? """
+
+files = os.listdir('data')
+for file in files:
+    if file.find('ratings_') == -1:
+        files.remove(file)
+data = pd.DataFrame(columns = ['one', 'two', 'tree', 'four', 'five'])
+for filename in files:
+    temp = pd.read_csv(os.path.join('data', filename), sep = ',',  names =['one', 'two', 'tree', 'four', 'five'])
+    data = pd.concat([data, temp], axis = 0)
+print (data)
