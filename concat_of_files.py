7a4fe876700df040176c3297cae53df7bcc1a88f
Dremora Restless draugr <drestles@e1r2p2.21-school.ru>
2019-03-05 04:10:01 +0300

    05.03.19 04:09

--- concat_of_files.py
+++ concat_of_files.py
@@ -10,8 +10,8 @@ files = os.listdir('datasets')
 for file in files:
     if file.find('ratings_') == -1:
         files.remove(file)
-data = pd.DataFrame(columns = ['one', 'two', 'tree', 'four', 'five'])
+data = pd.DataFrame(columns = ['one', 'two', 'tree', 'four'])
 for filename in files:
-    temp = pd.read_csv(os.path.join('datasets', filename), sep = ',',  names =['one', 'two', 'tree', 'four', 'five'])
+    temp = pd.read_csv(os.path.join('datasets', filename), sep = ',',  names =['one', 'two', 'tree', 'four'])
     data = pd.concat([data, temp], axis = 0)
 print (data)
--- films_rating.py
+++ films_rating.py
@@ -3,7 +3,7 @@ import pandas as pd
 """ Расчет и вывод рейтинга для каждого фильма
        на основе оценок всех пользователей """
 
-data = pd.read_csv('ratings.csv')
+data = pd.read_csv('datasets/ratings.csv')
 rating_of_movies = data.pivot_table(index = ['movieId', 'rating'],
     values = 'timestamp', aggfunc = 'count').reset_index()
 rating_of_movies['sum'] = rating_of_movies.groupby('movieId').timestamp.transform(lambda x: sum(x))
@@ -14,7 +14,7 @@ del rating_of_movies['timestamp']
 del rating_of_movies['sum']
 del rating_of_movies['mul']
 del rating_of_movies['sum_of_marks']
-movies = pd.read_csv('movies.csv')
+movies = pd.read_csv('datasets/movies.csv')
 rating_of_movies['movie'] = movies['title']
 rating_of_movies.drop_duplicates('movieId', inplace = True)
 print (rating_of_movies.head(700).sort_values('rating', ascending = False))
