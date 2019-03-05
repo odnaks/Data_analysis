14a88300de6a42f1d695a2b770983798826996f5
odnaks <43475234+odnaks@users.noreply.github.com>
2019-03-05 04:05:21 +0300

    Update concat_of_files.py

--- concat_of_files.py
+++ concat_of_files.py
@@ -6,12 +6,12 @@ from os.path import join, getsize
 файлов папки data в единый датафрейм data.
 Сколько строк получится в датафрейме data? """
 
-files = os.listdir('data')
+files = os.listdir('datasets')
 for file in files:
     if file.find('ratings_') == -1:
         files.remove(file)
 data = pd.DataFrame(columns = ['one', 'two', 'tree', 'four', 'five'])
 for filename in files:
-    temp = pd.read_csv(os.path.join('data', filename), sep = ',',  names =['one', 'two', 'tree', 'four', 'five'])
+    temp = pd.read_csv(os.path.join('datasets', filename), sep = ',',  names =['one', 'two', 'tree', 'four', 'five'])
     data = pd.concat([data, temp], axis = 0)
 print (data)
